d0b3de0dcb1bbcbfea1b6179ecc9130149945eb5
Varun-210806 <varuniyer337@gmail.com>
2026-01-26 01:18:58 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://xmysbeppsjgkvduhczyk.supabase.co";
-const SUPABASE_KEY = "sb_publishable_3aOPb_Xi3ijo16Niw2l1TQ_wX2ysWwq";
+const SUPABASE_URL = "https://oaejfnsqloujtikxbpqm.supabase.co";
+const SUPABASE_KEY = "sb_publishable_x1_VYFc15tyiwXNwytusIA_NQ9aYC2X";
 
 window.supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
